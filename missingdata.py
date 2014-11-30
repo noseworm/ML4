02b9b57e788b139213cce2b4b565c428593f967a
blasch <ben.italy@gmail.com>
2014-11-30 17:29:08 -0500

    Imputer
    Tried to implement Imputer, might be better to impl EM directly

--- missingdata.py
+++ missingdata.py
@@ -4,6 +4,7 @@ from sklearn import cross_validation
 from sklearn.linear_model import LinearRegression
 from sklearn.linear_model import Ridge
 from sklearn.preprocessing import normalize
+from sklearn.preprocessing import Imputer
 
 def loadData(filename):
 	data = np.genfromtxt(filename, delimiter=',', dtype=None, loose=True, invalid_raise=False, skip_header=1)
@@ -76,6 +77,39 @@ def dataFillMeanIndicator(data, feature_means):
 		new_set.append(np.array(dn))
 	return new_set
 				
+def dataFillImputer(data):
+	imp = Imputer(missing_values=0, strategy='most_frequent', axis=0)
+	new_data = splitData(data)
+	new_data = imp.fit_transform(np.array(new_data))
+	new_data = new_data.tolist()
+	data = rebuildData(data, new_data)
+	return data
+	
+def splitData(data):
+	new_set = []
+	for d in data:
+		new_d = []
+		for i in xrange(len(d)):
+			if(i in range(0,8) or i == 21):
+				new_d.append(d[i])
+		new_set.append(d)
+	return new_set
+			
+def rebuildData(data, new_data):
+	n = []
+	print len(data)
+	print len(new_data)
+	for j in xrange(len(data)):
+		new_d = []
+		for i in xrange(len(data[j])):
+			if(i in range(0,8)):
+				new_d.append(new_data[j][i])
+			elif(i == 21):
+				new_d.append(new_data[j][8])
+			else:
+				new_d.append(data[j][i])
+		n.append(new_d)
+	return n
 
 def extractOutput(data):
 	X = []
@@ -100,9 +134,12 @@ data = loadData("final_data_27.csv")
 # CHOOSE HOW TO HANDLE MISSING DATA 
 #nData = dataDeletion(data)
 #nData = dataFillMean(data,getMeanforFeatures(data))
-nData = dataFillMeanIndicator(data,getMeanforFeatures(data))
+#nData = dataFillMeanIndicator(data,getMeanforFeatures(data))
+#nData = dataFillImputer(data) #DOESNT WORK
+nData = data
+
 print len(nData)
-counts = getMissingDataCount(data)
+counts = getMissingDataCount(nData)
 print counts
 
 (X,Y) = extractOutput(nData)
