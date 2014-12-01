aee30d8c17620bb7c6ae5992c7f1a550d6400eaa
blasch <ben.italy@gmail.com>
2014-11-30 22:20:49 -0500

    update Exp
    updating exp

--- experiments.py
+++ experiments.py
@@ -4,6 +4,7 @@ from sklearn import cross_validation
 from sklearn.linear_model import LinearRegression
 from sklearn.linear_model import Ridge
 import missingdata
+import random
 
 '''
 Think it would be good to explore:
@@ -47,4 +48,32 @@ def getBestRidgeAlpha(alphas, mses):
 	index = mses.index(minError)
 	return alphas[index]
 
-kf = cross_validation.KFold(len(Y), n_folds=4)	
+def runKFold(X, Y, k):
+	kf = cross_validation.KFold(len(Y), n_folds=4)
+	errors = []
+	for train_index, test_index in kf:
+		X_train, X_test = X[train_index], X[test_index]
+		Y_train, Y_test = Y[train_index], Y[test_index]
+		mse = runLinearRegression(X_train, Y_train, X_test, Y_test)
+		errors.append(mse)
+	return np.mean(errors)
+
+def extractTestSet(X, Y, ratio):
+	expected_size = len(x)*ratio
+	test_set_X = []
+	test_set_Y = []
+	while(len(test_set_X) < expected_size):
+		chosen_index = random.randint(0, len(X))
+		test_set_X.append(X[chosen_index])
+		test_set_Y.append(Y[chosen_index])
+		del X[chosen_index]
+		del Y[chosen_index]
+	print "Length of TEST set = " + str(len(test_set_X))
+	print "Length of TRAIN set = " + str(len(X))
+	return (X, Y, test_set_X, test_set_Y)
+
+def finalTest(regressor, test_X, test_Y):
+	pred_Y = clf.predict(test_X)
+	mse = getMeanError(pred_Y, test_Y)
+	return mse
+
